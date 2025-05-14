7ecaa6f77644ba76402d382237b43215a22f1dc2
kfajutinamotin <motinkhristine@gmail.com>
2025-05-14 17:30:23 +0800

    Implemented font color change oriselection

--- script.js
+++ script.js
@@ -7,9 +7,17 @@ fontSelector.addEventListener("change", function(event){
 });
 
 const sizeSelector = document.getElementById("size-selector");
-fontSelector.addEventListener("change", function(event){
+colorSelector.addEventListener("change", function(event){
     const selectedSize= event.target.value;
     const textElement= document.getElementById("text");
     textElement.style.fontSize= selectedSize + "px";
     console.log(selectedSize);
+});
+
+const colorSelector = document.getElementById("color-selector");
+colorSelector.addEventListener("change", function(event){
+    const selectedColor= event.target.value;
+    const textElement= document.getElementById("text");
+    textElement.style.color= selectedColor;
+    console.log(selectedColor);
 });
