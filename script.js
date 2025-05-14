d57514d659dee2344d7e71e416457ebb6f47c8f4
kfajutinamotin <motinkhristine@gmail.com>
2025-05-14 17:23:45 +0800

    Implemented font family change oriselection

---
+++ script.js
@@ -0,0 +1,7 @@
+const fontSelector = document.getElementById("font-selector");
+fontSelector.addEventListener("change", function(event){
+    const selectedFont= event.target.value;
+    const textElement= document.getElementById("text");
+    textElement.style.fontFamily= selectedFont;
+    console.log(selectedFont);
+});
